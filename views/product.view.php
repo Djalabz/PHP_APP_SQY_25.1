d73306e04eaf80e4acf2e6d0fab0e3eb0d0383e1
djalabz <djalabz@gmail.com>
2024-01-30 15:05:27 +0100

    :bug: :lipstick: gitignore added and page product styled

--- views/product.view.php
+++ views/product.view.php
@@ -8,19 +8,18 @@ if (isset($_GET['product'])) {
 }
 
 ?>
+<div class="wrapper">
+    <h1>Page de produit</h1>
 
-<h1>Page de produit</h1>
-
-    <?php foreach($products as $product) : ?>
-        <?php if ($id == $product['id']) : ?>
-            <div class="product">
-                <a href="#"><img src="<?= $product['image'] ?>" alt=""></a>
-                <h2><?= $product['title'] ?></h2>
-                <h3><?= substr($product['description'], 0, 50) ?> ...</h3>
-                <h2><?= $product['price'] ?> €</h2>
-                <button><a>Ajouter au panier</a></button>
-            </div>
-        <?php endif ?>
-    <?php endforeach ?>
-
+        <?php foreach($products as $product) : ?>
+            <?php if ($id == $product['id']) : ?>
+                <div class="product">
+                    <a href="#"><img src="<?= $product['image'] ?>" alt=""></a>
+                    <h2><?= $product['title'] ?></h2>
+                    <h3><?= substr($product['description'], 0, 50) ?> ...</h3>
+                    <h2><?= $product['price'] ?> €</h2>
+                    <button><a>Ajouter au panier</a></button>
+                </div>
+            <?php endif ?>
+        <?php endforeach ?>
 </div>
