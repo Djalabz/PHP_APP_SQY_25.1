43be11f11318c288f0f0fc8d05154d562ecf312e
djalabz <djalabz@gmail.com>
2024-02-27 11:50:36 +0100

    :sparkles: todo html and css added

---
+++ scripts/exercices JS/index.js
@@ -0,0 +1,4 @@
+window.addEventListener('click', (e) => {
+    console.log(e)
+})
+
--- views/404.view.php
+++ views/404.view.php
@@ -4,6 +4,7 @@ include "partials/header.php";
 
 <h1>Ereur 404 : Page introuvable</h1>
 
+
 <?php 
 include "partials/footer.php"; 
 ?>
