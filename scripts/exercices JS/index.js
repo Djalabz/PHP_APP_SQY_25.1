5f747dbb5951823a9bdba1e8991557c6f9e5af07
djalabz <djalabz@gmail.com>
2024-02-27 12:03:08 +0100

    :lipstick: div replaced by ul todo in js

--- scripts/exercices JS/index.js
+++ scripts/exercices JS/index.js
@@ -1,4 +1,9 @@
-window.addEventListener('click', (e) => {
-    console.log(e)
-})
+// Récupérer les éléments HTML nécessaires
 
+
+// Ajouter un écouteur d'événements sur le bouton de soumission 
+
+// Récupérer la valeur de l'input que l'on injecte dans un élément HTML (par exemple li)
+// et qui s'ffichera dans notre ul de classe (output, cf le html)
+
+// Faire en sorte que l'on ne puisse pas ajouter de todos vides
